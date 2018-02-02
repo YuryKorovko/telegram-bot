36159448e0ac3f1319730be9bb625396893d2243
Yuriy-Korovko <korovko.ik@gmail.com>
2018-02-02 22:39:13 +0300

    -

--- bot/telegram.bot.js
+++ bot/telegram.bot.js
@@ -1,7 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 const df = require('dateformat');
 // replace the value below with the Telegram token you receive from @BotFather
-// const token = '517410530:AAEvpf2rDtfCfqUEwKrLDhOVHFBc30rZ9DE';
 
 // Create a bot that uses 'polling' to fetch new updates
 // const bot = new TelegramBot(token, { webHook: { port : port, host : host } });
